3e40ce6c839d5197cd010f2309f4524a7fbbf441
Diab16 <abdohdiab166@gmail.com>
2024-09-10 17:32:29 +0300

    create ApplicationDbContext

---
+++ Bill system API/Models/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Bill_system_API.Models
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Client> Client { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<InvoiceItem> InvoiceItems { get; set; }
+        public DbSet<Item> Items{ get; set; }
+        public DbSet<Type> Types{ get; set; }
+        public DbSet<Unit>Units  { get; set; }
+
+
+
+
+    }
+}
