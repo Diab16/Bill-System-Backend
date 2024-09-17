ff52455833de3fbd6ad4f118b96ada35efb7a94c
YoussefHamadaYH <youssefarqam772@gmail.com>
2024-09-17 23:29:45 +0300

    Modify : Add TypeId in TypeDTO

--- Bill system API/DTOs/TypeDTO.cs
+++ Bill system API/DTOs/TypeDTO.cs
@@ -2,6 +2,7 @@
 {
     public class TypeDTO
     {
+        public int TypeId { get; set; }
         public string TypeName { get; set; }
         public string? TypeNotes { get; set; }
         public string CompanyName { get; set; }
