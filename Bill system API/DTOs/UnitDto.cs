7687ac4c3d47b3b31a204feb2c70ff1ca53d9640
3bmo3ty <alaa.sleem.abdelsalam@vultara.com>
2024-09-18 18:39:31 +0300

    Branch merged

--- Bill system API/DTOs/UnitDto.cs
+++ Bill system API/DTOs/UnitDto.cs
@@ -1,9 +1,13 @@
-﻿namespace Bill_system_API.DTOs
+﻿using Bill_system_API.Models;
+
+namespace Bill_system_API.DTOs
 {
-    public class UnitDto
+    public class UnitDTO
     {
         public int Id { get; set; }
-
         public string Name { get; set; }
+        public string? Notes { get; set; }
+        //public ICollection<Item>? Items { get; set; }
+
     }
 }
