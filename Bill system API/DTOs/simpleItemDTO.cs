f6366a4c91917cf0961d0a788be0e1ca1d6d3fc3
Diab16 <abdohdiab166@gmail.com>
2024-09-28 23:40:35 +0300

    simpleItemDTO

---
+++ Bill system API/DTOs/simpleItemDTO.cs
@@ -0,0 +1,14 @@
+﻿using Bill_system_API.Validatioons;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bill_system_API.DTOs
+{
+    public class simpleItemDTO
+    {
+
+        public int ItemId { get; set; }
+        public string ItemName { get; set; }
+
+        public int ItemAvailableAmount { get; set; }
+    }
+}
