1a91f890a2a93d3b40d7dbfd3fb155456b4db5b3
Diab16 <abdohdiab166@gmail.com>
2024-09-29 03:30:22 +0300

    add more info to the simpleItemDTO

--- Bill system API/DTOs/simpleItemDTO.cs
+++ Bill system API/DTOs/simpleItemDTO.cs
@@ -8,7 +8,9 @@ namespace Bill_system_API.DTOs
 
         public int ItemId { get; set; }
         public string ItemName { get; set; }
-
         public int ItemAvailableAmount { get; set; }
+        public double ItemSellingPrice { get; set; }
+        public double ItemBuyingPrice { get; set; }
+
     }
 }
